b4adf2b9dbabce1fc80f89f6abbbc26324961244
Andrei <gagarin880120@gmail.com>
2021-01-06 14:46:33 +0300

    feat(back-27): add update user route

--- src/resources/user/user.router.js
+++ src/resources/user/user.router.js
@@ -4,7 +4,11 @@ const router = express.Router();
 
 const userController = require('./user.controller');
 
-router.route('/user').post(userController.registerUser).get(userController.getUser);
+router
+  .route('/user')
+  .post(userController.registerUser)
+  .get(userController.getUser)
+  .put(userController.updateUser);
 router.route('/user/login').post(userController.loginUser);
 router.route('/user/refresh').post(userController.updateToken);
 
