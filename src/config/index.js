78314b24c11b4f00ff286930834148ce498f545e
Andrei <gagarin880120@gmail.com>
2021-01-08 21:57:01 +0300

    fix: some minor changes

--- src/config/index.js
+++ src/config/index.js
@@ -2,22 +2,13 @@ const { config } = require('dotenv');
 
 const { parsed } = config();
 
-const {
-  PORT,
-  MODE,
-  DATA_BASE_NAME,
-  DATA_BASE_PASSWORD,
-  DATA_BASE_USERNAME,
-  MONGO_DB_CONNECTION_URL = `mongodb+srv://${DATA_BASE_USERNAME}:${DATA_BASE_PASSWORD}@cluster-mova-data-base.bnb39.mongodb.net/${DATA_BASE_NAME}?retryWrites=true&w=majority`,
-  IN_PROD = MODE !== 'prod',
-  SECRET_JWT_KEY
-} = parsed;
+const { PORT, MODE, IN_PROD = MODE !== 'prod', SECRET_JWT_KEY } = parsed;
 
 module.exports = {
   PORT,
   MODE,
-  DATA_BASE_NAME,
-  MONGO_DB_CONNECTION_URL,
+  MONGO_DB_CONNECTION_URL:
+    'mongodb+srv://admin:G8G2Bfs5cBepEdr@cluster-mova-data-base.bnb39.mongodb.net/mova-data-base-user?retryWrites=true&w=majority',
   IN_PROD,
   SECRET_JWT_KEY
 };
