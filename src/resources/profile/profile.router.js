dce83978f0baad13c9e984bdaf497d0cb1c90519
d90375 <d90375@gmail.com>
2021-01-08 22:20:01 +0300

    refactor: profile refactor

--- src/resources/profile/profile.router.js
+++ src/resources/profile/profile.router.js
@@ -4,66 +4,11 @@ const router = express.Router();
 
 const profileController = require('./profile.conroller');
 const { authByRole } = require('../../middlewares/jwtMiddleware');
-const { UR } = require('../../constants');
+const { UR, AR, MR } = require('../../constants');
 
-// router.route('/:username').get(
-//   catchErrors(async (req, res) => {
-//     const { username } = req.params;
-//     const user = await profileController.getUserByUsername(username);
-//
-//     if (!user) {
-//       throw new NotFound('User not found.');
-//     }
-//
-//     const { id } = user;
-//     const profile = await profileController.getProfileById(id);
-//     return res.status(200).json(profileSchema.toResponse(profile));
-//   })
-// );
-
-// router
-//   .route('/:username/follow')
-//   .post(
-//     catchErrors(async (req, res) => {
-//       const { username } = req.params;
-//       const user = await profileController.getUserByUsername(username);
-//
-//       if (!user) {
-//         throw new NotFound('User not found.');
-//       }
-//
-//       const { id } = user;
-//       const profile = await profileController.addFollower(id, req.body);
-//
-//       if (!profile) {
-//         throw new NotFound('Follower not found.');
-//       }
-//
-//       const updatedProfile = await profileController.getProfileById(id);
-//
-//       return res.status(200).json(profileSchema.toResponse(updatedProfile));
-//     })
-//   )
-//   .delete(
-//     catchErrors(async (req, res) => {
-//       const { username } = req.params;
-//       const user = await profileController.getUserByUsername(username);
-//
-//       if (!user) {
-//         throw new NotFound('User not found.');
-//       }
-//
-//       const { id } = user;
-//       const profile = await profileController.deleteFollower(id, req.body);
-//
-//       if (!profile) {
-//         throw new NotFound('Follower not found.');
-//       }
-//
-//       return res.status(200).json({ message: 'Follower successfully deleted.' });
-//     })
-//   );
-
-router.route('/:username/promote').post(authByRole(UR), profileController.promoteToModerator);
+router.route('/:username').get(authByRole([UR, MR, AR]), profileController.getProfile);
+router.route('/:username/follow').post(authByRole([UR, MR, AR]), profileController.followUser);
+router.route('/:username/follow').delete(authByRole([UR, MR, AR]), profileController.unFollowUser);
+router.route('/:username/promote').post(authByRole([AR]), profileController.promoteToModerator);
 
 module.exports = router;
