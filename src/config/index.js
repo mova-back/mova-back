1f989575661e72ec5fdb59186b330028a9f0525f
Andrei <gagarin880120@gmail.com>
2021-01-08 23:58:00 +0300

    fix: change config

--- src/config/index.js
+++ src/config/index.js
@@ -3,8 +3,16 @@ require('dotenv').config();
 module.exports = {
   PORT: process.env.PORT,
   MODE: process.env.MODE,
-  MONGO_DB_CONNECTION_URL:
+  BASE_URL: process.env.BASE_URL,
+  MDB_URL:
     'mongodb+srv://admin:G8G2Bfs5cBepEdr@cluster-mova-data-base.bnb39.mongodb.net/mova-data-base-user?retryWrites=true&w=majority',
-  IN_PROD: process.env.MODE !== 'prod',
-  SECRET_JWT_KEY: process.env.SECRET_JWT_KEY
+  COOKIE_SESSION_SECRET: process.env.COOKIE_SESSION_SECRET,
+  JWT_SECRET: process.env.JWT_SECRET,
+  EMAIL_HOST: process.env.EMAIL_HOST,
+  EMAIL_USERNAME: process.env.EMAIL_USERNAME,
+  EMAIL_PW: process.env.EMAIL_PW,
+  EMAIL_PORT: process.env.EMAIL_PORT,
+  REFRESH__EXP: process.env.REFRESH__EXP,
+  CODE_EXP: process.env.CODE_EXP,
+  ACCESS_EXP: process.env.ACCESS_EXP
 };
