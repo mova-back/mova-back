9510dddc1d5de7f098647e861c83fc9c9ade62bd
d90375 <d90375@gmail.com>
2021-01-08 20:32:33 +0300

    fix: role

--- src/middlewares/jwtMiddleware.js
+++ src/middlewares/jwtMiddleware.js
@@ -2,12 +2,13 @@ const { getBearerTokenFromRequest } = require('../utils/security/http');
 const { isValidToken } = require('../utils/security/jwt');
 const { Unauthorized } = require('../error');
 
-const authByRole = (role) => (req, resp, next) => {
+const authByRole = (listRole) => (req, resp, next) => {
   const token = getBearerTokenFromRequest(req);
 
-  const data = isValidToken(token);
-  if (data.role === role) {
-    req.userId = data.userId;
+  const currentUser = isValidToken(token);
+
+  if (listRole.filter((role) => role === currentUser.role)) {
+    req.userId = currentUser.userId;
     next();
   } else {
     throw new Unauthorized('JWT is not valid');
