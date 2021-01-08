dc5216214bece893b734fd5c958f8709fea26768
Andrei <gagarin880120@gmail.com>
2021-01-08 22:02:28 +0300

    fix: change config

--- src/config/index.js
+++ src/config/index.js
@@ -1,14 +1,10 @@
-const { config } = require('dotenv');
-
-const { parsed } = config();
-
-const { PORT, MODE, IN_PROD = MODE !== 'prod', SECRET_JWT_KEY } = parsed;
+require('dotenv').config();
 
 module.exports = {
-  PORT,
-  MODE,
+  PORT: process.env.PORT,
+  MODE: process.env.MODE,
   MONGO_DB_CONNECTION_URL:
     'mongodb+srv://admin:G8G2Bfs5cBepEdr@cluster-mova-data-base.bnb39.mongodb.net/mova-data-base-user?retryWrites=true&w=majority',
-  IN_PROD,
-  SECRET_JWT_KEY
+  IN_PROD: process.env.MODE !== 'prod',
+  SECRET_JWT_KEY: process.env.SECRET_JWT_KEY
 };
