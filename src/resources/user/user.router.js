3561d2adaa69438994b73a34325899e75d64d1b0
Andrei <gagarin880120@gmail.com>
2021-01-09 13:49:20 +0300

    fix(back-36): minor changes

--- src/resources/user/user.router.js
+++ src/resources/user/user.router.js
@@ -7,8 +7,8 @@ const userController = require('./user.controller');
 
 router
   .route('/user')
-  .post(userController.registerUser)
-  .get(userController.getUser, jwtMiddleware.auth);
+  .get(userController.getUser, jwtMiddleware.auth)
+  .post(userController.registerUser);
 router.route('/user/login').post(userController.loginUser);
 router.route('/user/refresh').post(userController.updateToken);
 router.route('/user/logout').post(userController.logout);
