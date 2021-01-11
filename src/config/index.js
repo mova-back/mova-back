e6c1707e15dc9f28e5169fa251be0b0ada5424ae
Andrei <gagarin880120@gmail.com>
2021-01-11 16:24:53 +0300

    fix: correct config

--- src/config/index.js
+++ src/config/index.js
@@ -7,11 +7,11 @@ module.exports = {
   MDB_URL:
     'mongodb+srv://admin:G8G2Bfs5cBepEdr@cluster-mova-data-base.bnb39.mongodb.net/mova-data-base-user?retryWrites=true&w=majority',
   COOKIE_SESSION_SECRET: process.env.COOKIE_SESSION_SECRET,
-  JWT_SECRET: 1234567890,
+  JWT_SECRET: '1234567890',
   EMAIL_HOST: 'smtp.ethereal.email',
   EMAIL_USERNAME: 'dean82@ethereal.email',
   EMAIL_PW: 'bRc47j8btyxNedz92Y',
-  EMAIL_PORT: 587,
+  EMAIL_PORT: '587',
   REFRESH__EXP: '60d',
   CODE_EXP: '10m',
   ACCESS_EXP: '30m'
