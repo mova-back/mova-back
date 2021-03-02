5e5e2fe0ec7092b2c2c4f7148b045d15d5bc4501
d90375 <d90375@gmail.com>
2021-03-02 22:41:58 +0300

    fix: refresh route public

--- src/resources/user/user.router.js
+++ src/resources/user/user.router.js
@@ -12,9 +12,7 @@ router
   .post(userController.registerUser)
   .put(jwtMiddleware.authByRole([UR, MR, AR]), userController.updateUser);
 router.route('/user/login').post(userController.loginUser);
-router
-  .route('/user/refresh')
-  .post(jwtMiddleware.authByRole([PRE_UR, UR, MR, AR]), userController.updateToken);
+router.route('/user/refresh').post(userController.updateToken);
 router
   .route('/user/logout')
   .post(jwtMiddleware.authByRole([PRE_UR, UR, MR, AR]), userController.logout);
