21955f855c2024749f8c36e159b4da4df48c3c76
d90375 <d90375@gmail.com>
2021-03-11 10:40:57 +0300

    feat: Content type middleware

---
+++ src/middlewares/ContentTypeMiddleware.js
@@ -0,0 +1,35 @@
+const { BaseMiddleware } = require('../root/BaseMiddleware');
+const { AppError } = require('../root/AppError');
+const { errorCodes } = require('../error/errorCodes');
+
+class ContentTypeMiddleware extends BaseMiddleware {
+  async init() {
+    console.log(`${this.constructor.name} initialized...`);
+  }
+
+  handler() {
+    return async (req, res, next) => {
+      try {
+        if (['POST', 'PUT', 'PATCH'].includes(req.method)) {
+          const contentType = req.headers['Content-Type'] || req.headers['content-type'];
+          if (!contentType) {
+            throw new AppError({ ...errorCodes.BAD_REQUEST, message: 'Please provide content-type' });
+          }
+
+          const validContentType = ['application/json', 'multipart/form-data'];
+          const isValidContentType = contentType.includes('application/json') || contentType.includes('multipart/form-data');
+
+          if (!isValidContentType) {
+            throw new AppError({ ...errorCodes.BAD_REQUEST, message: `Invalid content type. Expect one of: [${validContentType}]` });
+          }
+        }
+
+        next();
+      } catch (error) {
+        next(error);
+      }
+    };
+  }
+}
+
+module.exports = { ContentTypeMiddleware };
