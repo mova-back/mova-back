c640a055d6942ef7c7bc855b95aed7d9d230a969
d90375 <d90375@gmail.com>
2021-03-15 11:19:08 +0300

    feat: add  api

--- src/resources/modules/users/UsersController.js
+++ src/resources/modules/users/UsersController.js
@@ -5,22 +5,22 @@ const { BaseController } = require('../../../root');
 
 class UsersController extends BaseController {
   get router() {
-    router.get('/users', this.actionRunner(actions.ListUsersAction));
-    router.get('/user/current', this.actionRunner(actions.GetCurrentUserAction));
-    router.get('/user/:id', this.actionRunner(actions.GetUserByIdAction));
-    router.post('/user', this.actionRunner(actions.CreateUserAction));
-    router.patch('/user', this.actionRunner(actions.UpdateUserAction));
-    router.delete('/user/:id', this.actionRunner(actions.RemoveUserAction));
+    router.get('api/users', this.actionRunner(actions.ListUsersAction));
+    router.get('api/user/current', this.actionRunner(actions.GetCurrentUserAction));
+    router.get('api/user/:id', this.actionRunner(actions.GetUserByIdAction));
+    router.post('api/user', this.actionRunner(actions.CreateUserAction));
+    router.patch('api/user', this.actionRunner(actions.UpdateUserAction));
+    router.delete('api/user/:id', this.actionRunner(actions.RemoveUserAction));
 
-    router.post('/user/change-password', this.actionRunner(actions.ChangePasswordAction));
-    router.post('/user/send-reset-password-email', this.actionRunner(actions.SendResetPasswordEmailAction));
-    router.post('/user/reset-password', this.actionRunner(actions.ResetPasswordAction));
+    router.post('api/user/change-password', this.actionRunner(actions.ChangePasswordAction));
+    router.post('api/user/send-reset-password-email', this.actionRunner(actions.SendResetPasswordEmailAction));
+    router.post('api/user/reset-password', this.actionRunner(actions.ResetPasswordAction));
 
-    router.post('/user/confirm-registration', this.actionRunner(actions.ConfirmRegistrationAction));
-    router.post('/user/change-email', this.actionRunner(actions.ChangeEmailAction));
-    router.post('/user/confirm-email', this.actionRunner(actions.ConfirmEmailAction));
-    router.post('/user/resend-confirm-new-email-token', this.actionRunner(actions.ResendConfirmNewEmailTokenAction));
-    router.post('/user/cancel-email-changing', this.actionRunner(actions.CancelEmailChangingAction));
+    router.post('api/user/confirm-registration', this.actionRunner(actions.ConfirmRegistrationAction));
+    router.post('api/user/change-email', this.actionRunner(actions.ChangeEmailAction));
+    router.post('api/user/confirm-email', this.actionRunner(actions.ConfirmEmailAction));
+    router.post('api/user/resend-confirm-new-email-token', this.actionRunner(actions.ResendConfirmNewEmailTokenAction));
+    router.post('api/user/cancel-email-changing', this.actionRunner(actions.CancelEmailChangingAction));
 
     return router;
   }
