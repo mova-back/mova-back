19ba2273e88beaa7240f3b169a2dacd361048c8e
d90375 <d90375@gmail.com>
2021-03-16 12:48:05 +0300

    fix: query middleware

--- src/middlewares/QueryMiddleware.js
+++ src/middlewares/QueryMiddleware.js
@@ -9,21 +9,21 @@ class QueryMiddleware extends BaseMiddleware {
   handler() {
     return async (req, res, next) => {
       // set default query
-      req.query =
-        req.method === 'GET'
-          ? {
-              ...req.query,
-              page: Number(req.query.page) || 0,
-              limit: Number(req.query.limit) || 10,
-              filter: req.query.filter || {},
-              orderBy: {
-                ...((req.query.orderBy && req.query.orderBy.field && { field: req.query.orderBy.field }) || { field: 'createdAt' }),
-                ...((req.query.orderBy && req.query.orderBy.direction && { direction: req.query.orderBy.direction }) || {
-                  direction: 'asc',
-                }),
-              },
-            }
-          : { ...req.query };
+      // req.query =
+      //   req.method === 'GET'
+      //     ? {
+      //         ...req.query,
+      //         page: Number(req.query.page) || 0,
+      //         limit: Number(req.query.limit) || 10,
+      //         filter: req.query.filter || {},
+      //         orderBy: {
+      //           ...((req.query.orderBy && req.query.orderBy.field && { field: req.query.orderBy.field }) || { field: 'createdAt' }),
+      //           ...((req.query.orderBy && req.query.orderBy.direction && { direction: req.query.orderBy.direction }) || {
+      //             direction: 'asc',
+      //           }),
+      //         },
+      //       }
+      //     : { ...req.query };
 
       next();
     };
