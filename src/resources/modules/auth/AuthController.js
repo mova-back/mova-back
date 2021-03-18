ae657c4e092c72df8d8e69ed7af790bdb4b33fc8
d90375 <d90375@gmail.com>
2021-03-18 15:04:16 +0300

    fix: route refresh token

--- src/resources/modules/auth/AuthController.js
+++ src/resources/modules/auth/AuthController.js
@@ -7,7 +7,7 @@ class AuthController extends BaseController {
   get router() {
     router.post('/api/auth/login', this.actionRunner(actions.LoginAction));
     router.post('/api/auth/logout', this.actionRunner(actions.LogoutAction));
-    router.post('/api/auth/refresh-tokens', this.actionRunner(actions.RefreshTokensAction));
+    router.post('/api/auth/refresh-token', this.actionRunner(actions.RefreshTokensAction));
 
     return router;
   }
