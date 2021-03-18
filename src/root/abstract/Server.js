b987cce621009aa31599c83c869a3195f1d7d5f4
d90375 <d90375@gmail.com>
2021-03-18 14:13:08 +0300

    heroku fix

--- src/root/abstract/Server.js
+++ src/root/abstract/Server.js
@@ -101,7 +101,7 @@ function start({ port, host, controllers, middlewares, ErrorMiddleware, cookieSe
       process.exit(1);
     });
 
-    return app.listen(port, host, () => resolve({ port, host }));
+    return app.listen(port || 4400);
   });
 }
 
